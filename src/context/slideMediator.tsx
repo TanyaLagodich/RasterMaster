1465084a330dc4aae1e2e4d9ff2dfc94b1c9e92f
Tatiana Lagodich <t.lagodich@amarkets.com>
2024-11-15 10:14:47 +0400

    feat: support localStorage

--- src/context/slideMediator.tsx
+++ src/context/slideMediator.tsx
@@ -1,12 +1,29 @@
-import { createContext, useRef, useState } from 'react';
+import { createContext, useRef, useState, useEffect } from 'react';
 import { SlideMediator } from '@/mediator';
-import { Slide,SlideMediatorContextType  } from '@/types';
+import { Slide as SlideType, SlideMediatorContextType  } from '@/types';
+import { SlideFactory } from '@/factories/slide';
 
 export const SlideMediatorContext = createContext<SlideMediatorContextType | null>(null);
 
 export const SlideMediatorProvider = ({ children }) => {
-    const [slides, setSlides] = useState<Slide[]>([]);
-    const [currentSlide, setCurrentSlide] = useState<Slide | null>(null);
+    const [slides, setSlides] = useState<SlideType[]>([]);
+    const [currentSlide, setCurrentSlide] = useState<SlideType | null>(null);
+
+    const reconstructSlide = (slideData: any): SlideType => {
+        return Object.assign(SlideFactory.createSlide(), slideData);
+    }
+
+    useEffect(() => {
+        const storedSlides = localStorage.getItem('slides');
+        if (storedSlides) {
+            setSlides(JSON.parse(storedSlides).map((slide) => reconstructSlide(slide)));
+        }
+    
+        const storedCurrentSlide = localStorage.getItem('currentSlide');
+        if (storedCurrentSlide) {
+            setCurrentSlide(reconstructSlide(JSON.parse(storedCurrentSlide)));
+        }
+    }, []);
 
     const mediatorRef = useRef<SlideMediator | null>(null);
 
@@ -17,6 +34,18 @@ export const SlideMediatorProvider = ({ children }) => {
     }
 
     mediatorRef.current.setSlidesList(slides);
+
+    useEffect(() => {
+        localStorage.setItem('slides', JSON.stringify(slides));
+    }, [slides]);
+
+    useEffect(() => {
+        if (currentSlide) {
+            localStorage.setItem('currentSlide', JSON.stringify(currentSlide));
+        } else {
+            localStorage.removeItem('currentSlide');
+        }
+    }, [currentSlide]);
 
     return (
         <SlideMediatorContext.Provider value={{ mediator: mediatorRef.current, slides, currentSlide }}>
